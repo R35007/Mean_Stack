b32f757927700c20f980046c854262a4d6fbe5de
Siva <Mycount.siva@gmail.com>
2018-11-25 14:59:54 +0530

    formated

--- server/routes/api.js
+++ server/routes/api.js
@@ -3,89 +3,91 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const Video = require('../models/video')
 
-mongoose.connect('mongodb://localhost/videoplayer', (err)=>{
-    if(err){
-        console.log(`error ${err}`);
-    }
+mongoose.connect('mongodb://localhost/videoplayer', (err) => {
+  if (err) {
+    console.log(`error ${err}`);
+  }
 })
 
-router.get('/videos', function(req,res){
-    // res.send('Express api worked');
-    console.log('Get Request for all videos');
-    Video.find({})
-    .exec((err,videos)=>{
-        if(err){
-            console.log('Error retriving videos');
-        }else{
-            res.json(videos);
-        }
+router.get('/videos', function (req, res) {
+  // res.send('Express api worked');
+  console.log('Get Request for all videos');
+  Video.find({})
+    .exec((err, videos) => {
+      if (err) {
+        console.log('Error retriving videos');
+      } else {
+        res.json(videos);
+      }
     })
 });
 
-router.get('/videos/:id', function(req,res){
-    // res.send('Express api worked');
-    console.log('Get Request for single Video');
-    Video.findById(req.params.id)
-    .exec((err,video)=>{
-        if(err){
-            console.log('Error retriving videos');
-        }else{
-            res.json(video);
-        }
+router.get('/videos/:id', function (req, res) {
+  // res.send('Express api worked');
+  console.log('Get Request for single Video');
+  Video.findById(req.params.id)
+    .exec((err, video) => {
+      if (err) {
+        console.log('Error retriving videos');
+      } else {
+        res.json(video);
+      }
     })
 });
 
-router.post('/videos', function(req,res){
-    // res.send('Express api worked');
-    console.log('Get Request for single Video');
+router.post('/videos', function (req, res) {
+  // res.send('Express api worked');
+  console.log('Get Request for single Video');
 
-    let newVideo = new Video();
-    newVideo.title = req.body.title;
-    newVideo.url = req.body.url;
-    newVideo.description = req.body.description;
+  let newVideo = new Video();
+  newVideo.title = req.body.title;
+  newVideo.url = req.body.url;
+  newVideo.description = req.body.description;
 
-    newVideo.save((err,video)=>{
-        if(err){
-            console.log('Error retriving videos');
-        }else{
-            res.json(video);
-        }
+  newVideo.save((err, video) => {
+    if (err) {
+      console.log('Error retriving videos');
+    } else {
+      res.json(video);
+    }
+  })
+});
+
+router.put('/videos/:id', function (req, res) {
+  // res.send('Express api worked');
+  console.log('Get Request for single Video');
+
+  Video.findByIdAndUpdate(
+    req.params.id, {
+      $set: {
+        title: req.body.title,
+        url: req.body.url,
+        description: req.body.description
+      }
+    }, {
+      new: true
+    },
+    (err, video) => {
+      if (err) {
+        console.log('Error retriving videos');
+      } else {
+        res.json(video);
+      }
     })
 });
 
-router.put('/videos/:id', function(req,res){
-    // res.send('Express api worked');
-    console.log('Get Request for single Video');
+router.delete('/videos/:id', function (req, res) {
+  // res.send('Express api worked');
+  console.log('Get Request for single Video');
 
-    Video.findByIdAndUpdate(
-        req.params.id,
-        {
-            $set:{title: req.body.title, url: req.body.url, description: req.body.description}
-        },
-        {
-            new: true
-        },
-        (err,video)=>{
-        if(err){
-            console.log('Error retriving videos');
-        }else{
-            res.json(video);
-        }
-    })
-});
-
-router.delete('/videos/:id', function(req,res){
-    // res.send('Express api worked');
-    console.log('Get Request for single Video');
-
-    Video.findByIdAndRemove(
-        req.params.id,
-        (err,video)=>{
-        if(err){
-            console.log('Error retriving videos');
-        }else{
-            res.json(video);
-        }
+  Video.findByIdAndRemove(
+    req.params.id,
+    (err, video) => {
+      if (err) {
+        console.log('Error retriving videos');
+      } else {
+        res.json(video);
+      }
     })
 });
 
